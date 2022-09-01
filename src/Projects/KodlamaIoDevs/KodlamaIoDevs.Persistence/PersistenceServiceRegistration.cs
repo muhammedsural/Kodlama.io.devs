420e621c68dc2d5b94cd663c856b4af63b039330
Muhammed Sural <muhammedsural@gmail.com>
2022-09-01 15:19:25 +0300

    Created Project

---
+++ src/Projects/KodlamaIoDevs/KodlamaIoDevs.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+
+namespace Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+                                                                IConfiguration configuration)
+        {
+            //services.AddDbContext<BaseDbContext>(options => options.UseSqlServer(@"Server=DESKTOP-8212T7;Database=KodlamaIoDevsDb;UID=msural;PWD=sural6177;"));
+            services.AddDbContext<BaseDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("KodlamaIoDevsConnectionString")));
+
+
+
+            return services;
+        }
+    }
+}
